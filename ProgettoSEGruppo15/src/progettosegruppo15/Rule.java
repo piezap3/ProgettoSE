fba64e0b431c2c730f6b5f8c45784b86191ecc35
MarcoSellitto <151437717+MarcoSellitto@users.noreply.github.com>
2023-11-22 17:50:39 +0100

    Rule Class and Enum for Activity Type
    Upload of Rule Class and Activity Type Enumeration

--- ProgettoSEGruppo15/src/progettosegruppo15/Rule.java
+++ ProgettoSEGruppo15/src/progettosegruppo15/Rule.java
@@ -1,1 +1,31 @@
-test
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progettosegruppo15;
+
+/**
+ * Class that implements single rules.
+ * @author Marco
+ */
+public class Rule {
+    private Trigger trigger;
+    private Action action;
+    private EnumActivityType activityType;
+    private int sleepTime; // in seconds
+
+    /**
+     * Constructor for Rule class
+     * @param trigger: trigger of rule
+     * @param action: action to run when the trigger activates
+     * @param activityType: type of the rule
+     * @param sleepTime: sleepTime of rule (in seconds)
+     */
+    public Rule(Trigger trigger, Action action, EnumActivityType activityType, int sleepTime) {
+        this.trigger = trigger;
+        this.action = action;
+        this.activityType = activityType;
+        this.sleepTime = sleepTime;
+    }
+}
